0aef4066bbd27a77b3cfa40b6f634cd87301e088
Elvys Rodriguez <elvyster@gmail.com>
2024-08-22 21:50:43 -0400

    Añadidos los resultados del scraping al archivo Resultado.txt

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL de la página web que quieres hacer scraping
+url = 'https://www.yelu.do/leidsa/results/loto-mas'
+
+# Realizar la petición HTTP
+response = requests.get(url)
+
+# Verificar si la petición fue exitosa
+if response.status_code == 200:
+    # Parsear el contenido HTML de la página
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+     # Abrir el archivo en modo escritura dentro del bloque with
+    with open('Resultado.txt', 'w', encoding='utf-8') as file:
+    
+        # Buscar todas las filas de la tabla dentro del div con clase "desc"
+        rows = soup.select('div.desc tr')
+        
+        for row in rows:
+            # Obtener el td con title="Fecha del Sorteo"
+            fecha_td = row.select_one('td[title="Fecha del Sorteo"]')
+            
+            # Verificar que exista el td y obtener el contenido
+            if fecha_td:
+                fecha_texto = fecha_td.decode_contents().strip()
+                
+                # Obtener todos los span dentro del row
+                spans = row.select('span[title]')
+                for span in spans:
+                    span_text = span['title']
+                    # Concatenar el contenido
+                    resultado = f"{fecha_texto} - {span_text}"
+                    # Escribir el resultado en el archivo
+                    file.write(resultado + '\n')        
+
+                    print('Resultado:', resultado)
+else:
+    print(f"Error al acceder a la página: {response.status_code}")
